afb91fbe9822125c5e3af67059fea6b04e3fb28f
Vaios0x <vaiogioss@gmail.com>
2025-09-03 18:39:17 -0600

    Hacer modal de AppKit más pequeño: reducir ancho a 350px y altura a 520px

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next/types'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import './appkit-modal.css'
 import { Navbar } from '@/components/layout/navbar'
 import { Footer } from '@/components/layout/footer'
 import { headers } from 'next/headers'
